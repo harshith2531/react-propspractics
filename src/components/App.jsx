070d05c0c1e0ef35878caa242841fa7884b7612f
harshith2531 <106176595+harshith2531@users.noreply.github.com>
2022-06-13 12:49:22 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App(props) {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img
+            className="card img circle-img"
+            src={props.img}
+            alt="avatar_img"
+          />
+        </div>
+        <div className="bottom ">
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
